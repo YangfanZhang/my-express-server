744afda63976d599eae9cd75531cba1eaf660713
Yangfan Zhang <yf_chang@icloud.com>
2020-06-16 16:52:34 +1000

    Added some pages.

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(request, response){
-    response.send("Hello");
+//a method that allows us to specify that what should happen when a browser makes a get request to out server.
+//first parameter, represents location.
+//A callback function to tell the server 
+app.get("/", function(req, res){
+    res.send("<h1>Hello</h1>");
 })
 
-app.get("/contact", function(request, response){
-    response.send("contact");
+app.get("/contact", function(req, res){
+    res.send("contact");
 })
 
-app.get("/home", function(request, response){
-    response.send("I am Hermione");
+app.get("/about", function(req, res){
+    res.send("Something about me");
 })
 
-
+app.get("/home", function(req, res){
+    res.send("I am Hermione");
+})
 
 app.listen(3000, function(){
     console.log("server starts on port 3000");
 });
+
